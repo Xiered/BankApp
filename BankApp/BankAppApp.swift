20ea80683bb3649dce8e556eea2db2864c1a2245
Xiered <134545303+Xiered@users.noreply.github.com>
2023-09-26 05:13:42 +0300

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Дмитрий Герасимов on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
